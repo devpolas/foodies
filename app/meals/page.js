7f84ee614b324a602fa9bbf035b7d84928a166de
devpolas <195308698+devpolas@users.noreply.github.com>
2025-08-18 20:07:17 +0600

    install sqlite data base on this project

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,25 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function page() {
   return (
-    <main>
-      <h1>Meals</h1>
-      <Link href='/meals/meal-1'>Meals - 1</Link>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>{" "}
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It's easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href='/meals/share'>Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
