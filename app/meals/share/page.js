e22e7bf840da84d3f5a3aa2c8e50bba87537ddaa
devpolas <195308698+devpolas@users.noreply.github.com>
2025-08-19 22:45:28 +0600

    capable to create a new meal

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,7 @@
-import ImagePicker from '@/components/meals/image-picker';
-import classes from './page.module.css';
+import ImagePicker from "@/components/meals/image-picker";
+import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions";
+import MealFormSubmit from "@/components/meals/meal-form-submit";
 
 export default function ShareMealPage() {
   return (
@@ -11,37 +13,37 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
-              <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <label htmlFor='name'>Your name</label>
+              <input type='text' id='name' name='name' required />
             </p>
             <p>
-              <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <label htmlFor='email'>Your email</label>
+              <input type='email' id='email' name='email' required />
             </p>
           </div>
           <p>
-            <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
+            <label htmlFor='title'>Title</label>
+            <input type='text' id='title' name='title' required />
           </p>
           <p>
-            <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
+            <label htmlFor='summary'>Short Summary</label>
+            <input type='text' id='summary' name='summary' required />
           </p>
           <p>
-            <label htmlFor="instructions">Instructions</label>
+            <label htmlFor='instructions'>Instructions</label>
             <textarea
-              id="instructions"
-              name="instructions"
-              rows="10"
+              id='instructions'
+              name='instructions'
+              rows='10'
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker name='image' label='Your Image' />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealFormSubmit />
           </p>
         </form>
       </main>
